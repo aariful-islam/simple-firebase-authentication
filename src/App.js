42dae49c34deb11fe70279165fb23d9d33ece644
arif99050 <arifulislam99050@gmail.com>
2022-04-07 00:03:50 +0600

    show user details on ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import app from './firebase.init';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
   const provider =new GoogleAuthProvider();
+  const [user, setuser]=useState({})
+
   const handleGoogleSignIn=()=>{
     signInWithPopup(auth,provider)
     .then(res=>{
+      setuser(res.user)
       console.log(res.user)
 
     })
@@ -21,6 +25,8 @@ function App() {
     
     <div className="App">
       <button onClick={handleGoogleSignIn}>sign in</button>
+      <h2>{user.displayName}</h2>
+      <p>{user.email}</p>
       
      
     </div>
